b300e5eb811275031c166b43cd843e3f5fb2eb50
Trigon1015 <zz2464@nyu.edu>
2025-04-13 22:53:45 -0400

    hw9 submit

--- Assets/IRONHEAD Games/Scripts/ButtonInteraction.cs
+++ Assets/IRONHEAD Games/Scripts/ButtonInteraction.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonInteraction : MonoBehaviour
 {
     public void OnButtonPressed()
     {
-        Debug.Log("Button Pressed");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
